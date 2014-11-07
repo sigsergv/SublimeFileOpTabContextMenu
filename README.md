98d8d8cc375b0af4f57419a2efc852bc5aa01f06
Sergey Stolyarov <sergei@regolit.com>
2014-11-07 23:01:13 +0600

    Closed Issue #2

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ useful command to tab context menu. Here they are:
 * **Clone tab** — clone active tab, the same as menu *File → New View into file*;
 * **Rename file...** — renames a file opened in the active tab
 * **Find in sidebar** — reveals file in the sidebar (project files tree)
+* **Clone file contentx** — create a new file and copy there content of currenlty opened file
 
--- tab_conext_menu.py
+++ tab_conext_menu.py
@@ -8,3 +8,9 @@ class RenameFileInTabCommand(sublime_plugin.TextCommand):
             return
 
         self.view.window().run_command('rename_path', args={'paths': [file_name]})
+
+class CloneFileContents(sublime_plugin.TextCommand):
+    def run(self, edit, args=None, **kwargs):
+        content = self.view.substr(sublime.Region(0, self.view.size()))
+        new_view = self.view.window().new_file()
+        new_view.insert(edit, 0, content)
