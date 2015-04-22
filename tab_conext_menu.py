66854fd4fb790208151931194c1b000a64f3f31a
Sergey Stolyarov <sergei@regolit.com>
2015-04-22 11:41:21 +0600

    Fixed issue #3
    Use tab under cursor, not active one

--- tab_conext_menu.py
+++ tab_conext_menu.py
@@ -1,16 +1,46 @@
 import sublime, sublime_plugin
 
 class RenameFileInTabCommand(sublime_plugin.TextCommand):
-    def run (self, edit, args=None, **kwargs):
+    def run (self, edit, args=None, index=-1, group=-1, **kwargs):
+        w = self.view.window()
+        views = w.views_in_group(group)
+        view = views[index]
         
-        file_name = self.view.file_name()
+        file_name = view.file_name()
         if file_name is None:
             return
 
-        self.view.window().run_command('rename_path', args={'paths': [file_name]})
+        w.run_command('rename_path', args={'paths': [file_name]})
 
-class CloneFileContents(sublime_plugin.TextCommand):
-    def run(self, edit, args=None, **kwargs):
-        content = self.view.substr(sublime.Region(0, self.view.size()))
-        new_view = self.view.window().new_file()
+
+class CloneFileContentsCommand(sublime_plugin.TextCommand):
+    def run(self, edit, args=None, index=-1, group=-1, **kwargs):
+        w = self.view.window()
+        views = w.views_in_group(group)
+        view = views[index]
+
+        content = view.substr(sublime.Region(0, view.size()))
+        new_view = w.new_file()
+        w.set_view_index(new_view, group, index+1)
         new_view.insert(edit, 0, content)
+
+
+class FileopCloneFile(sublime_plugin.TextCommand):
+    def run(self, edit, args=None, index=-1, group=-1, **kwargs):
+        w = self.view.window()
+        views = w.views_in_group(group)
+        view = views[index]
+
+        w.focus_view(view)
+        w.run_command('clone_file')
+
+
+class FileopRevealInSideBar(sublime_plugin.TextCommand):
+    def run(self, edit, args=None, index=-1, group=-1, **kwargs):
+        w = self.view.window()
+        views = w.views_in_group(group)
+        view = views[index]
+
+        w.focus_view(view)
+        w.run_command('reveal_in_side_bar')
+
